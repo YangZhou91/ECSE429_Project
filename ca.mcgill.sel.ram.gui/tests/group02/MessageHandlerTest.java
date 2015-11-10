d91d6d455e7d8e2214da630e82c27e86161e36a4
Yang Zhou <gavinzhou10c@gmail.com>
2015-11-09 21:11:56 -0500

    [Add] Add two unit test for handlers

---
+++ ca.mcgill.sel.ram.gui/tests/group02/MessageHandlerTest.java
@@ -0,0 +1,69 @@
+/**
+ * 
+ */
+package group02;
+
+import static org.junit.Assert.fail;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author Yang Zhou
+ *         ID: 260401719
+ *
+ */
+public class MessageHandlerTest {
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+    }
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+    }
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    /**
+     * Test method for
+     * {@link ca.mcgill.sel.ram.ui.views.message.handler.impl.MessageHandler#processTapAndHoldEvent(org.mt4j.input.inputProcessors.componentProcessors.tapAndHoldProcessor.TapAndHoldEvent)}
+     * .
+     */
+    @Test
+    public void testProcessTapAndHoldEvent() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for
+     * {@link ca.mcgill.sel.ram.ui.views.handler.BaseHandler#processGestureEvent(org.mt4j.input.inputProcessors.MTGestureEvent)}
+     * .
+     */
+    @Test
+    public void testProcessGestureEvent() {
+        fail("Not yet implemented");
+    }
+
+}
---
+++ ca.mcgill.sel.ram.gui/tests/group02/MessageViewHandlerTest.java
@@ -0,0 +1,129 @@
+/**
+ * 
+ */
+package group02;
+
+import static org.junit.Assert.fail;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author Yang Zhou
+ *         ID:260401719
+ *
+ */
+public class MessageViewHandlerTest {
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+    }
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+    }
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    /**
+     * Test method for
+     * {@link ca.mcgill.sel.ram.ui.views.message.handler.impl.MessageViewHandler#processUnistrokeEvent(org.mt4j.input.inputProcessors.componentProcessors.unistrokeProcessor.UnistrokeEvent)}
+     * .
+     */
+    @Test
+    public void testProcessUnistrokeEvent() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for
+     * {@link ca.mcgill.sel.ram.ui.views.message.handler.impl.MessageViewHandler#handleUnistrokeGesture(ca.mcgill.sel.ram.ui.views.AbstractView, org.mt4j.input.inputProcessors.componentProcessors.unistrokeProcessor.UnistrokeUtils.UnistrokeGesture, org.mt4j.util.math.Vector3D, org.mt4j.input.inputProcessors.componentProcessors.unistrokeProcessor.UnistrokeEvent)}
+     * .
+     */
+    @Test
+    public void testHandleUnistrokeGesture() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for
+     * {@link ca.mcgill.sel.ram.ui.views.message.handler.impl.MessageViewHandler#processWheelEvent(ca.mcgill.sel.ram.ui.events.WheelEvent)}
+     * .
+     */
+    @Test
+    public void testProcessWheelEvent() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for
+     * {@link ca.mcgill.sel.ram.ui.views.message.handler.impl.MessageViewHandler#processZoomEvent(org.mt4j.input.inputProcessors.componentProcessors.zoomProcessor.ZoomEvent)}
+     * .
+     */
+    @Test
+    public void testProcessZoomEvent() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for
+     * {@link ca.mcgill.sel.ram.ui.views.message.handler.impl.MessageViewHandler#handleCreateFragment(ca.mcgill.sel.ram.ui.views.message.MessageViewView, ca.mcgill.sel.ram.ui.views.message.LifelineView, org.mt4j.util.math.Vector3D, ca.mcgill.sel.ram.FragmentContainer)}
+     * .
+     */
+    @Test
+    public void testHandleCreateFragment() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for
+     * {@link ca.mcgill.sel.ram.ui.views.handler.impl.AbstractViewHandler#processGestureEvent(org.mt4j.input.inputProcessors.MTGestureEvent)}
+     * .
+     */
+    @Test
+    public void testProcessGestureEvent() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for
+     * {@link ca.mcgill.sel.ram.ui.views.handler.impl.AbstractViewHandler#processDragEvent(org.mt4j.input.inputProcessors.componentProcessors.dragProcessor.DragEvent)}
+     * .
+     */
+    @Test
+    public void testProcessDragEvent() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for
+     * {@link ca.mcgill.sel.ram.ui.views.handler.impl.AbstractViewHandler#processPanEvent(org.mt4j.input.inputProcessors.componentProcessors.panProcessor.PanEvent)}
+     * .
+     */
+    @Test
+    public void testProcessPanEvent() {
+        fail("Not yet implemented");
+    }
+
+}
