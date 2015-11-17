7cf7aace23f70b258176b89cea4e7563eff71d2a
Wayne Lee <wayneleeyk@users.noreply.github.com>
2015-11-16 23:14:04 -0500

    added test method for createLifeline and createLifelineWithMessage (and testCreateLifelineWithMessage02 needs work)

--- ca.mcgill.sel.ram.controller/tests/group02/ca/mcgill/sel/ram/controller/MessageViewControllerTest.java
+++ ca.mcgill.sel.ram.controller/tests/group02/ca/mcgill/sel/ram/controller/MessageViewControllerTest.java
@@ -9,12 +9,21 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import ca.mcgill.sel.commons.emf.util.AdapterFactoryRegistry;
+import ca.mcgill.sel.commons.emf.util.EMFModelUtil;
 import ca.mcgill.sel.commons.emf.util.ResourceManager;
 import ca.mcgill.sel.ram.Aspect;
+import ca.mcgill.sel.ram.Classifier;
+import ca.mcgill.sel.ram.CombinedFragment;
+import ca.mcgill.sel.ram.FragmentContainer;
 import ca.mcgill.sel.ram.Interaction;
+import ca.mcgill.sel.ram.InteractionFragment;
 import ca.mcgill.sel.ram.Lifeline;
 import ca.mcgill.sel.ram.MessageView;
+import ca.mcgill.sel.ram.Operation;
+import ca.mcgill.sel.ram.RamFactory;
 import ca.mcgill.sel.ram.RamPackage;
+import ca.mcgill.sel.ram.Reference;
+import ca.mcgill.sel.ram.TypedElement;
 import ca.mcgill.sel.ram.provider.RamItemProviderAdapterFactory;
 import ca.mcgill.sel.ram.util.RAMModelUtil;
 import ca.mcgill.sel.ram.util.RamResourceFactoryImpl;
@@ -90,23 +99,150 @@ public class MessageViewControllerTest {
      */
     @Test
     public void testCreateLifeline() {
-        MessageView messageView = (MessageView) aspect.getMessageViews().get(0);
+        // One test case can cover all du-paths
+        // 68-69-70-72
+        Classifier classA = aspect.getStructuralView().getClasses().get(0);
+        Operation doSomething = classA.getOperations().get(0);
+        
+        MessageView messageView = RAMModelUtil.getMessageViewFor(aspect, doSomething);
         Interaction owner = messageView.getSpecification();
-        Lifeline self = owner.getLifelines().get(0);
+        
+        TypedElement classB = classA.getAssociationEnds().get(0);
         int lifelineCount = owner.getLifelines().size();
 
-        // Creates a new lifeline of itself
-        controller.createLifeline(owner, self.getRepresents(), 0, 0);
+        controller.createLifeline(owner, classB, 0, 0);
         assertEquals(lifelineCount + 1, owner.getLifelines().size());
     }
 
     /**
+     * Need to include the following transition for the paths for all-uses (or all-p-uses-some-c?) <br>
+     * 93-101-102, 93-101-104 (var = container) <br>
+     * 102-107, 104-107 (var = previousFragment) <br>
+     * 107-112-113, 107-112-116 (var = initialMessage) <br>
+     * 93-*-121-123, 93-*-121-144 (var = represents) <br>
+     * 123-*-133-134, 123-*-133-137 (var = staticOperation) <br>
+     * 93-*-159-160, 93-*-159-170 (var = owner, container) <br>
+     * 148-*-159-163-164, 148-*-159-163-166 (var = newlifeline) <br>
+     * <br>
+     * Need 3 test cases <br>
+     * 1) 93-101-102-107-112-116-121-123-133-134-148-159-160-163-164 <br>
+     * 2) 93-101-104-107-112-113-116-121-123-133-137-148-159-160-163-166 <br>
+     * 3) 93-101-102-107-112-116-121-144-159-170 <br>
+     * <br>
+     *  Test path 1: 93-101-102-107-112-116-121-123-133-134-148-159-160-163-164 <br>
+     * <br>
      * Test method for {@link MessageViewController#createLifelineWithMessage
      * (Interaction, TypedElement, float, float, Lifeline, FragmentContainer, Operation, int)}.
      */
     @Test
-    public void testCreateLifelineWithMessage() {
-        fail("Not yet implemented");
+    public void testCreateLifelineWithMessage01() {
+        // container.getFragments.size > 0
+        // initialMessage != null
+        // represents is Reference and represents.container is null (is metaclass)
+        // staticOperation == true
+        // owner != container 
+        // newlifeline not covered by combined fragments
+        
+        Classifier classA = aspect.getStructuralView().getClasses().get(0);
+        Operation doSomething2 = classA.getOperations().get(1);
+        
+        MessageView messageView = RAMModelUtil.getMessageViewFor(aspect, doSomething2);
+        Interaction owner = messageView.getSpecification();
+        
+        Classifier classC = aspect.getStructuralView().getClasses().get(2);
+        Operation getRandom = classC.getOperations().get(1);        
+        Reference classCType = RamFactory.eINSTANCE.createReference();
+        classCType.setType(classC);
+        
+        Lifeline lifelineFrom = owner.getLifelines().get(0);
+        
+        CombinedFragment cf = (CombinedFragment) owner.getFragments().get(1);
+        FragmentContainer container = cf.getOperands().get(0);
+        int previousFragIndex = container.getFragments().size() - 1;
+        
+        int previousLifelineCount = owner.getLifelines().size();
+        int previousMessageCount = owner.getMessages().size();
+        
+        controller.createLifelineWithMessage(owner, classCType, 0, 0, lifelineFrom, 
+                container, getRandom, previousFragIndex);
+        
+        assertEquals(owner.getLifelines().size(), previousLifelineCount + 1);
+        assertEquals(owner.getMessages().size(), previousMessageCount + 1);
+    }
+    
+    /**
+     * Test path 2: 93-101-104-107-112-113-116-121-123-133-137-148-159-160-163-166.
+     * @see #testCreateLifelineWithMessage01
+     */
+    @Test
+    public void testCreateLifelineWithMessage02() {
+        // container.getFragments.size == 0
+        // initialMessage == null
+        // represents is Reference and represents.container is null (is metaclass)
+        // staticOperation == false
+        // owner != container 
+        // newlifeline covered by combined fragments
+        
+        Classifier classA = aspect.getStructuralView().getClasses().get(0);
+        Operation doSomething2 = classA.getOperations().get(1);
+        
+        MessageView messageView = RAMModelUtil.getMessageViewFor(aspect, doSomething2);
+        Interaction owner = messageView.getSpecification();
+        
+        Classifier classC = aspect.getStructuralView().getClasses().get(2);
+        Operation create = classC.getOperations().get(0);        
+        Reference classCType = RamFactory.eINSTANCE.createReference();
+        classCType.setType(classC);
+        
+        Lifeline lifelineFrom = owner.getLifelines().get(0);
+        
+        // TODO Find a fragment combination such that initial message is null
+        // TODO Find a way to make newlifeline covered by combined fragments
+        
+        CombinedFragment cf = (CombinedFragment) owner.getFragments().get(5);
+        FragmentContainer container = cf.getOperands().get(0);
+        
+        int addAtIndex = 0;
+        
+        int previousLifelineCount = owner.getLifelines().size();
+        int previousMessageCount = owner.getMessages().size();
+        
+        controller.createLifelineWithMessage(owner, classCType, 0, 0, lifelineFrom, 
+                container, create, addAtIndex);
+        
+        assertEquals(owner.getLifelines().size(), previousLifelineCount + 1);
+        assertEquals(owner.getMessages().size(), previousMessageCount + 1);
+    }
+    
+    /**
+     * Test path 3: 93-101-102-107-112-116-121-144-159-170.
+     */
+    @Test
+    public void testCreateLifelineWithMessage03() {
+        // container.getFragments.size > 0
+        // initialMessage != null
+        // represents is Reference and represents.container is not null (not metaclass)
+        // owner == container 
+        
+        Classifier classA = aspect.getStructuralView().getClasses().get(0);
+        Operation doSomething = classA.getOperations().get(0);
+        
+        MessageView messageView = RAMModelUtil.getMessageViewFor(aspect, doSomething);
+        Interaction owner = messageView.getSpecification();
+        
+        Classifier classB = classA.getAssociationEnds().get(0).getClassifier();
+        TypedElement classBType = classA.getAssociationEnds().get(0);
+        Lifeline lifelineFrom = owner.getLifelines().get(0);
+        int addAtIndex = 0;
+        Operation signature = classB.getOperations().get(0);
+        
+        int previousLifelineCount = owner.getLifelines().size();
+        int previousMessageCount = owner.getMessages().size();
+        
+        controller.createLifelineWithMessage(owner, classBType, 0, 0, lifelineFrom, owner, signature, addAtIndex);
+        
+        assertEquals(owner.getLifelines().size(), previousLifelineCount + 1);
+        assertEquals(owner.getMessages().size(), previousMessageCount + 1);
     }
 
     /**
