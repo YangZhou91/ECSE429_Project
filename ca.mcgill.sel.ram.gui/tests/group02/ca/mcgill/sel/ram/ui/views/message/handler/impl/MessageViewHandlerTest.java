e64491bc028f438b6f013777e4aee74138bfbb0b
Wayne Lee <wayneleeyk@users.noreply.github.com>
2015-11-22 23:24:01 -0500

    Added some tests for processUnistrokeEvent

--- ca.mcgill.sel.ram.gui/tests/group02/ca/mcgill/sel/ram/ui/views/message/handler/impl/MessageViewHandlerTest.java
+++ ca.mcgill.sel.ram.gui/tests/group02/ca/mcgill/sel/ram/ui/views/message/handler/impl/MessageViewHandlerTest.java
@@ -7,18 +7,30 @@ import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.mt4j.components.visibleComponents.shapes.MTPolygon;
+import org.mt4j.input.inputData.InputCursor;
+import org.mt4j.input.inputData.MTFingerInputEvt;
+import org.mt4j.input.inputProcessors.MTGestureEvent;
+import org.mt4j.input.inputProcessors.componentProcessors.unistrokeProcessor.UnistrokeEvent;
 import org.mt4j.sceneManagement.ISceneChangeListener;
 import org.mt4j.sceneManagement.SceneChangeEvent;
+import org.mt4j.util.math.Vertex;
 
 import ca.mcgill.sel.commons.emf.util.AdapterFactoryRegistry;
 import ca.mcgill.sel.commons.emf.util.ResourceManager;
 import ca.mcgill.sel.ram.Aspect;
+import ca.mcgill.sel.ram.Classifier;
+import ca.mcgill.sel.ram.Interaction;
+import ca.mcgill.sel.ram.MessageView;
+import ca.mcgill.sel.ram.Operation;
 import ca.mcgill.sel.ram.RamPackage;
 import ca.mcgill.sel.ram.provider.RamItemProviderAdapterFactory;
 import ca.mcgill.sel.ram.ui.RamApp;
 import ca.mcgill.sel.ram.ui.scenes.DisplayAspectScene;
+import ca.mcgill.sel.ram.ui.views.message.MessageViewView;
 import ca.mcgill.sel.ram.ui.views.message.handler.IMessageViewHandler;
 import ca.mcgill.sel.ram.ui.views.message.handler.MessageViewHandlerFactory;
+import ca.mcgill.sel.ram.util.RAMModelUtil;
 import ca.mcgill.sel.ram.util.RamResourceFactoryImpl;
 
 /**
@@ -117,14 +129,207 @@ public class MessageViewHandlerTest {
     }
 
     /**
+     * Test case 1: Gesture is not started, ended nor updated. <br>
      * Test method for {@link ca.mcgill.sel.ram.ui.views.message.handler.impl.MessageViewHandler
      * #processUnistrokeEvent(org.mt4j.input.inputProcessors.componentProcessors.unistrokeProcessor.UnistrokeEvent)}.
      */
     @Test
-    public void testProcessUnistrokeEvent() {
-        fail("Not yet implemented");
+    public void testProcessUnistrokeEvent01() {
+        UnistrokeEvent event = new UnistrokeEvent(null, MTGestureEvent.GESTURE_CANCELED, null, null, null, null);
+        handler.processUnistrokeEvent(event);
+        
+        // Nothing should have changed
+    }
+    
+    /**
+     * Test case 2: Gesture is started. <br>
+     * @throws InterruptedException if interrupted
+     * @see #testProcessUnistrokeEvent01()
+     */
+    @Test
+    public void testProcessUnistrokeEvent02() throws InterruptedException {
+        RamApp app = RamApp.getApplication();
+        Classifier classA = aspect.getStructuralView().getClasses().get(0);
+        Operation doSomething1 = classA.getOperations().get(0);
+        final MessageView messageView = RAMModelUtil.getMessageViewFor(aspect, doSomething1);        
+        final DisplayAspectScene aspectScene = (DisplayAspectScene) app.getCurrentScene();
+        
+        // Changes the scene
+        app.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                aspectScene.showMessageView(messageView);
+                appNotify();
+            }
+        });
+        unitTestWait();
+        
+        Vertex[] vertices = new Vertex[1];
+        vertices[0] = new Vertex();
+        MTPolygon visualization = new MTPolygon(app, vertices);
+        MessageViewView mvv = (MessageViewView) aspectScene.getCurrentView();
+        UnistrokeEvent event = new UnistrokeEvent(null, MTGestureEvent.GESTURE_STARTED, 
+                mvv, visualization, null, null);
+        handler.processUnistrokeEvent(event);
+        
+        // TODO: unistroke layer has one more child
+    }
+    
+    /**
+     * Test case 3: Gesture is updated but too short. <br>
+     * @throws InterruptedException if interrupted.
+     * @see #testProcessUnistrokeEvent01()
+     */
+    @Test
+    public void testProcessUnistrokeEvent03() throws InterruptedException {
+        RamApp app = RamApp.getApplication();
+        Classifier classA = aspect.getStructuralView().getClasses().get(0);
+        Operation doSomething1 = classA.getOperations().get(0);
+        final MessageView messageView = RAMModelUtil.getMessageViewFor(aspect, doSomething1);        
+        final DisplayAspectScene aspectScene = (DisplayAspectScene) app.getCurrentScene();
+        
+        // Changes the scene
+        app.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                aspectScene.showMessageView(messageView);
+                appNotify();
+            }
+        });
+        unitTestWait();
+        
+        MessageViewView mvv = (MessageViewView) aspectScene.getCurrentView();
+        InputCursor cursor = new InputCursor();
+        cursor.getEvents().add(new MTFingerInputEvt(null, 0, 0, 0, cursor));        
+        cursor.getEvents().add(new MTFingerInputEvt(null, 1, 0, 0, cursor));        
+        UnistrokeEvent event = new UnistrokeEvent(null, MTGestureEvent.GESTURE_UPDATED, 
+                mvv, null, null, cursor);
+        handler.processUnistrokeEvent(event);
+        
+        // TODO: Nothing should have happened
+    }
+    
+    /**
+     * Test case 4: Gesture is updated and good length. <br>
+     * @throws InterruptedException if interrupted
+     * @see #testProcessUnistrokeEvent01()
+     */
+    @Test
+    public void testProcessUnistrokeEvent04() throws InterruptedException {
+        RamApp app = RamApp.getApplication();
+        Classifier classA = aspect.getStructuralView().getClasses().get(0);
+        Operation doSomething1 = classA.getOperations().get(0);
+        final MessageView messageView = RAMModelUtil.getMessageViewFor(aspect, doSomething1);        
+        final DisplayAspectScene aspectScene = (DisplayAspectScene) app.getCurrentScene();
+        
+        // Changes the scene
+        app.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                aspectScene.showMessageView(messageView);
+                appNotify();
+            }
+        });
+        unitTestWait();
+        
+        MessageViewView mvv = (MessageViewView) aspectScene.getCurrentView();
+        InputCursor cursor = new InputCursor();
+        cursor.getEvents().add(new MTFingerInputEvt(null, 0, 0, 0, cursor));        
+        cursor.getEvents().add(new MTFingerInputEvt(null, 6, 0, 0, cursor));        
+        UnistrokeEvent event = new UnistrokeEvent(null, MTGestureEvent.GESTURE_UPDATED, 
+                mvv, null, null, cursor);
+        handler.processUnistrokeEvent(event);
+        
+        // Nothing happens, but lifeline highlighted maybe
+        // TODO: don't know who to check that
+    }
+    
+    /**
+     * Test case 5: Gesture is ended and does not reaches a lifeline. <br>
+     * @throws InterruptedException if interrupted
+     * @see #testProcessUnistrokeEvent01()
+     */
+    @Test
+    public void testProcessUnistrokeEvent05() throws InterruptedException {
+        RamApp app = RamApp.getApplication();
+        Classifier classA = aspect.getStructuralView().getClasses().get(0);
+        Operation doSomething1 = classA.getOperations().get(0);
+        final MessageView messageView = RAMModelUtil.getMessageViewFor(aspect, doSomething1);        
+        final DisplayAspectScene aspectScene = (DisplayAspectScene) app.getCurrentScene();
+        
+        // Changes the scene
+        app.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                aspectScene.showMessageView(messageView);
+                appNotify();
+            }
+        });
+        unitTestWait();
+        
+        MessageViewView mvv = (MessageViewView) aspectScene.getCurrentView();
+        InputCursor cursor = new InputCursor();
+        cursor.getEvents().add(new MTFingerInputEvt(null, 160, 180, 0, cursor));        
+        cursor.getEvents().add(new MTFingerInputEvt(null, 260, 180, 0, cursor));        
+        final UnistrokeEvent event = new UnistrokeEvent(null, MTGestureEvent.GESTURE_ENDED, 
+                mvv, null, null, cursor);
+        
+        // Process the event
+        app.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                handler.processUnistrokeEvent(event);
+                appNotify();
+            }
+        });
+        unitTestWait();
+        
+        // TODO: Added component in aspectScene
     }
 
+    /**
+     * Test case 6: Gesture is ended and reaches a lifeline. <br>
+     * @throws InterruptedException if interrupted
+     * @see #testProcessUnistrokeEvent01()
+     */
+    @Test
+    public void testProcessUnistrokeEvent06() throws InterruptedException {
+        RamApp app = RamApp.getApplication();
+        Classifier classA = aspect.getStructuralView().getClasses().get(0);
+        Operation doSomething2 = classA.getOperations().get(1);
+        final MessageView messageView = RAMModelUtil.getMessageViewFor(aspect, doSomething2);        
+        final DisplayAspectScene aspectScene = (DisplayAspectScene) app.getCurrentScene();
+        
+        // Changes the scene
+        app.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                aspectScene.showMessageView(messageView);
+                appNotify();
+            }
+        });
+        unitTestWait();
+        
+        MessageViewView mvv = (MessageViewView) aspectScene.getCurrentView();
+        InputCursor cursor = new InputCursor();
+        cursor.getEvents().add(new MTFingerInputEvt(null, 160, 230, 0, cursor));        
+        cursor.getEvents().add(new MTFingerInputEvt(null, 390, 230, 0, cursor));        
+        final UnistrokeEvent event = new UnistrokeEvent(null, MTGestureEvent.GESTURE_ENDED, 
+                mvv, null, null, cursor);
+        
+        // Process the event
+        app.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                handler.processUnistrokeEvent(event);
+                appNotify();
+            }
+        });
+        unitTestWait();
+        
+        // TODO: Added component in aspectScene
+    }
+    
     /**
      * Test method for {@link ca.mcgill.sel.ram.ui.views.message.handler.impl.MessageViewHandler
      * #handleUnistrokeGesture(ca.mcgill.sel.ram.ui.views.AbstractView, 
